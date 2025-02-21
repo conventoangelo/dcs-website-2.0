910b876e5315c5a272782f2cabad02b3b3fad9e2
Angelo Convento <convento.angelo@gmail.com>
2025-02-21 13:07:06 +0800

    feat: include laboratory names in categoy people cards

--- website-frontend/src/routes/people/[slug]/+page.server.ts
+++ website-frontend/src/routes/people/[slug]/+page.server.ts
@@ -1,7 +1,5 @@
 /** @type {import('./$types').PageServerLoad} */
 import { readItems } from '@directus/sdk';
-import { parse } from 'valibot';
-import { People } from '$lib/models/people';
 import getDirectusInstance from '$lib/directus';
 import { error } from '@sveltejs/kit';
 
@@ -25,19 +23,16 @@ export async function load({ params, fetch }) {
 
 	const category = categories[0];
 
-	const people = parse(
-		People,
-		await directus.request(
-			readItems('people', {
-				filter: {
-					category: {
-						_eq: category.title
-					}
+	const people = await directus.request(
+		readItems('people', {
+			filter: {
+				category: {
+					_eq: category.title
 				}
-			})
-		)
+			},
+			fields: ['*', 'affiliations.laboratories_id.*']
+		})
 	);
-
 	return {
 		category,
 		people
