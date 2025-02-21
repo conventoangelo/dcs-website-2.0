9374b9198fc9239c367074f7018a552977fbf0c4
Angelo Convento <convento.angelo@gmail.com>
2025-02-21 12:46:13 +0800

    feat: add affiliation data to people

--- website-frontend/src/routes/people/+page.server.ts
+++ website-frontend/src/routes/people/+page.server.ts
@@ -1,14 +1,24 @@
 /** @type {import('./$types').PageServerLoad} */
 import { readItems, readSingleton } from '@directus/sdk';
 import { parse } from 'valibot';
-import { People } from '$lib/models/people';
 import { PeopleOverview } from '$lib/models/people_overview';
 import getDirectusInstance from '$lib/directus';
 
 export async function load({ fetch }) {
 	const directus = getDirectusInstance(fetch);
+	const people = await directus.request(
+		readItems('people', {
+			fields: ['*', 'affiliations.laboratories_id.*']
+		})
+	);
+	console.log(people);
+	const people_overview = parse(
+		PeopleOverview,
+		await directus.request(readSingleton('people_overview'))
+	);
+
 	return {
-		people: parse(People, await directus.request(readItems('people'))),
-		people_overview: parse(PeopleOverview, await directus.request(readSingleton('people_overview')))
+		people,
+		people_overview
 	};
 }
